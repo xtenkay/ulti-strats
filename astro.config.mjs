7f6d5329058a534d9a0fc34e407ea7f9abef84a7
Tenkay <tenkay.tenkai@gmail.com>
2024-05-14 11:47:28 -0300

    base removal

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://xtenkay.github.io',
-  	base: '/NA-ultimate-strats',
 	integrations: [
 		starlight({
 			title: 'NA Ultimate Strats',
